68702f50f4f367e675b977663b0d51bcae21df31
faraponoff <faraponoff@mail.ru>
2021-04-18 20:26:49 +0430

    18.04.21

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +9,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat kitty =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(kitty.getName());
+        Cat pussy =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(pussy.getName());
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(kitty == pussy);
     }
 }
